4cdda1d99d0f2963dfae0474dc8e788d6a36bc72
Mulu <brianmulu007@gmail.com>
2022-10-17 13:37:42 +0300

    Done

--- contacts.rb
+++ contacts.rb
@@ -18,7 +18,19 @@ def contacts
 end
 
 def remove_strawberry(contacts)
-  # your code here!
+  # Step 1: Iterate over the first level
+  contacts.each do |person, contact_details_hash|
+    # Step 2. Iterate over the second level
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        # Step 3. Locate the element we're looking for
+        if attribute == :favorite_ice_cream_flavors
+          # Step 4. Update the hash
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
 
 # print the output to the terminal for inspection
